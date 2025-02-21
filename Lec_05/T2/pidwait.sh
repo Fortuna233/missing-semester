dacd6d4347a0b4cb79f754760a6c9252ed6a5fbc
Fortuna233 <1311722974@qq.com>
2025-02-21 10:51:53 +0800

    02211051

--- Lec_05/T2/pidwait.sh
+++ Lec_05/T2/pidwait.sh
@@ -1,8 +1,12 @@
 pidwait()
 {
-   while !(kill $1) # 循环直到进程结束
-   do
-   sleep 1 
-   done
-   ls
+   while 0; do  # 循环直到进程结束
+		 x=$(pgrep $1)
+		 if [ ${x} -eq "" ]; then
+			 echo "finished"
+			 break
+		 fi
+	   sleep 1
+	 done
+	 ls
 }
